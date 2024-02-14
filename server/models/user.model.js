8bbcd6d52ac7f5a7a6b245aa3189c39f56bd7174
Sun He <hsun67@my.centennialcollege.ca>
2024-02-14 14:15:07 -0500

    week6 add Authentication

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose'
 //const mongoose = require('mongoose');
+import crypto from 'crypto'
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -30,9 +31,9 @@ const UserSchema = new mongoose.Schema({
 UserSchema.virtual('password')
   .set(function (password) {
     this._password = password;
-    //this.salt = this.makeSalt();
-    this.hashed_password = password;
-    //this.hashed_password = this.encryptPassword(password);
+    this.salt = this.makeSalt();
+    // this.hashed_password = password;
+    this.hashed_password = this.encryptPassword(password);
   })
   .get(function () {
     return this._password;
@@ -45,5 +46,25 @@ UserSchema.path('hashed_password').validate(function (v) {
     this.invalidate('password', 'Password is required');
   }
 }, null);
+UserSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password
+  },
+  encryptPassword: function (password) {
+    if (!password) return ''
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex')
+    } catch (err) {
+      return ''
+    }
+  },
+  makeSalt: function () {
+    return Math.round((new Date().valueOf() * Math.random())) + ''
+  }
+}
+
 //module.exports = mongoose.model('User', UserSchema);
 export default mongoose.model('User', UserSchema);
