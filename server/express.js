6ceea820509d1be5c4f2471763cb9737c86927f6
Sun He <hsun67@my.centennialcollege.ca>
2024-03-07 10:21:48 -0500

    front-end

--- server/express.js
+++ server/express.js
@@ -7,12 +7,17 @@ import helmet from 'helmet'
 import Template from './../template.js'
 import userRoutes from './routes/user.routes.js'
 import authRoutes from './routes/auth.routes.js'
+import path from 'path'
+
 const app = express()
+const CURRENT_WORKING_DIR = process.cwd()
+
 //...
 app.get('/', (req, res) => {
     res.status(200).send(Template())
 })
 //...
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', userRoutes)
@@ -31,5 +36,6 @@ app.use((err, req, res, next) => {
         console.log(err)
     }
 })
+
 
 export default app
